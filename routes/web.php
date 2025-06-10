703a849662b2b136df098e910d0105a3bb20d2e5
unknown <leonardocampo94@gmail.com>
2025-06-09 23:29:39 -0300

    :sparkles: feat: Implements router redirect

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/produtos');
 
 Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
 
